8365953cbbc9d7b46e5ff3f6d2852ce59b32065e
Ahmed Samir <ahmed.samir@microengineering.ca>
2023-06-03 02:14:30 +0300

    database structure and migrations

---
+++ database/migrations/2023_06_02_195042_create_trips_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('vehicle_id')->constrained('vehicles')->cascadeOnDelete();
+            $table->foreignId('route_id')->constrained('vehicles')->cascadeOnDelete();
+            $table->timestamp('departure_time')->nullable();;
+            $table->timestamp('arrival_time')->nullable();;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
